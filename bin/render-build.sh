1349096b66c1402e8deefbe070081b21027c0a44
ArkSentinel <nicolasgc99@outlook.com>
2026-02-05 13:56:02 -0300

    v1.6.2

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 bundle install
 bin/rails assets:precompile
 bin/rails assets:clean
 bin/rails db:migrate
-# Opcional: bin/rails db:seed (si quieres que los usuarios se creen solos al subirlo)
+
+# ESTA ES LA LÍNEA QUE NECESITAS:
+bin/rails db:seed
